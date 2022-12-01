2c24f0eef79c81593839c3d462e0109f6527a337
Marc Reisner <mreisner@epic.com>
2022-12-01 09:19:57 -0600

    Day 1 parts 1 and 2

--- src/Day1.jl
+++ src/Day1.jl
@@ -1,9 +1,19 @@
 module Day1
 
+function parseelves(input::String)
+    return map(elf -> map(food -> parse(Int, strip(food)), filter(food -> food != "", split(elf, "\n"))), split(input, "\n\n"))
+end
+
 function solvepart1(input::String)
+    elves = parseelves(input)
+    return maximum(map(sum, elves))
 end
 
 function solvepart2(input::String)
+    elves = parseelves(input)
+    sums = map(sum, elves)
+    topthree = sort(sums, rev=true)[1:3]
+    return sum(topthree)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,22 @@ using Test
 
 @testset "Day 1" begin
     Day1 = include("../src/Day1.jl")
-    @test Day1.solvepart1("") === nothing
-    @test Day1.solvepart2("") === nothing
+    testinput = """
+          1000
+          2000
+          3000
+
+          4000
+
+          5000
+          6000
+
+          7000
+          8000
+          9000
+
+          10000
+      """
+    @test Day1.solvepart1(testinput) === 24000
+    @test Day1.solvepart2(testinput) === 45000
 end
