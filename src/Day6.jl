d62228fb4a6d8ef606dcc3855afc74cd992c08b1
Marc Reisner <mreisner@epic.com>
2022-12-06 10:27:45 -0600

    Day 6 parts 1 and 2

--- src/Day6.jl
+++ src/Day6.jl
@@ -1,9 +1,27 @@
 module Day6
 
+function solve(input::String, markerlength::Int64)
+    window = ""
+
+    for (i, char) in enumerate(input)
+        window *= char
+
+        if length(window) == markerlength
+            if allunique(window)
+                return i
+            end
+
+            window = window[2:end]
+        end
+    end
+end
+
 function solvepart1(input::String)
+    return solve(input, 4)
 end
 
 function solvepart2(input::String)
+    return solve(input, 14)
 end
 
 end
--- test/Day6-tests.jl
+++ test/Day6-tests.jl
@@ -1,7 +1,8 @@
 @testset "Day 6" begin
     Day6 = include("../src/Day6.jl")
     testinput = """
+    mjqjpqmgbljsphdztnvjfqwrcgsmlb
     """
     @test Day6.solvepart1(testinput) === 7
-    @test Day6.solvepart2(testinput) === nothing
+    @test Day6.solvepart2(testinput) === 19
 end
